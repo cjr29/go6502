0ff7d288d05969764b020a024780306e0678cccb
Chris Riddick <chrisriddick@gmail.com>
2024-05-24 21:58:32 -0400

    Add Help and Run buttons. Add GUI clock.

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -1,3 +1,13 @@
+// Copyright 2024 Christopher J. Riddick
+//
+// This package creates a GUI for monitoring and controlling CPU simulators
+// It uses the fyne.io package which is cross-compatible with Linux, macOS, and Windows
+// The gui window is started from the main go routine by calling dashboard.New with
+// arguments including pointers to the CPU structure and host as well as functions to
+// implement the GUI button responses. The call to dashboard.New returns a pointer to
+// a fyne.Window and a pointer to a console buffer (bytes.Buffer) that implements the
+// buffered io insterface. This buffer is used by the main program to redirect
+// stdout to the GUI console.
 package dashboard
 
 import (
@@ -9,7 +19,6 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/cjr29/go6502/cpu"
 	"github.com/cjr29/go6502/host"
@@ -34,31 +43,31 @@ var (
 	consoleLabel          *widget.Label
 	commandLine           *widget.Entry
 	commandContainer      *fyne.Container
-	//inputCPUClock         *widget.Entry
-	loadButton        *widget.Button
-	runButton         *widget.Button
-	stepButton        *widget.Button
-	resetButton       *widget.Button
-	pauseButton       *widget.Button
-	exitButton        *widget.Button
-	submitButton      *widget.Button
-	currentTime       *widget.Label
-	mainContainer     *fyne.Container
-	buttonsContainer  *fyne.Container
-	settingsContainer *fyne.Container
-	statusContainer   *fyne.Container
-	registerContainer *fyne.Container
-	consoleContainer  *container.Scroll
-	stackContainer    *fyne.Container
-	speedContainer    *fyne.Container
-	centerContainer   *fyne.Container
-	middleContainer   *fyne.Container
+	loadButton            *widget.Button
+	runButton             *widget.Button
+	stepButton            *widget.Button
+	resetButton           *widget.Button
+	pauseButton           *widget.Button
+	exitButton            *widget.Button
+	helpButton            *widget.Button
+	submitButton          *widget.Button
+	currentTime           *widget.Label
+	mainContainer         *fyne.Container
+	buttonsContainer      *fyne.Container
+	settingsContainer     *fyne.Container
+	statusContainer       *fyne.Container
+	registerContainer     *fyne.Container
+	consoleContainer      *container.Scroll
+	stackContainer        *fyne.Container
+	centerContainer       *fyne.Container
+	middleContainer       *fyne.Container
 )
 
 var Console = container.NewVBox()
 var ConsoleScroller = container.NewVScroll(Console)
 
-func New(cpu *cpu.CPU, host *host.Host, submit func(), reset func(), load func(), step func(), run func(), pause func(), exit func()) (w fyne.Window, o *bytes.Buffer) {
+func New(cpu *cpu.CPU, host *host.Host, submit func(), reset func(), load func(), step func(),
+	run func(), pause func(), exit func(), help func()) (w fyne.Window, o *bytes.Buffer) {
 
 	c = cpu  // All data comes from the CPU structure object
 	h = host // Structure that manages the specific CPU implementation
@@ -79,31 +88,10 @@ func New(cpu *cpu.CPU, host *host.Host, submit func(), reset func(), load func()
 	pauseButton = widget.NewButton("Pause", pause)
 	exitButton = widget.NewButton("Exit", exit)
 	submitButton = widget.NewButton("Submit", submit)
+	helpButton = widget.NewButton("Help", help)
 
 	// Display time
 	currentTime = widget.NewLabel("")
-
-	// Clock settings line
-	/* 	inputCPUClock = widget.NewEntry()
-	   	inputCPUClock.SetText("1")
-	   	speedContainer = container.NewHBox(
-	   		canvas.NewText("Clock Speed = ", color.Black),
-	   		inputCPUClock,
-	   		canvas.NewText("ms  ", color.Black),
-	   		widget.NewButton("Save", func() {
-	   			if s, err := strconv.ParseFloat(inputCPUClock.Text, 64); err == nil {
-	   				if s <= 1.0 {
-	   					cpu.Clock = 1 // ticker requires positive value >= 1
-	   				} else {
-	   					cpu.Clock = s
-	   				}
-	   			}
-	   			SetStatus(fmt.Sprintf("Clock set to %f milliseconds", cpu.Clock))
-	   		}),
-	   		canvas.NewText("Set clock speed in milliseconds. 1.0 sets clock to full speed.  ", color.Black),
-	   		layout.NewSpacer(),
-	   	)
-	*/
 
 	// Command entry line
 	commandLine = widget.NewEntry()
@@ -160,13 +148,12 @@ func New(cpu *cpu.CPU, host *host.Host, submit func(), reset func(), load func()
 		stepButton,
 		pauseButton,
 		exitButton,
-		layout.NewSpacer(),
+		helpButton,
 		currentTime,
 	)
 
 	settingsContainer = container.NewVBox(
 		buttonsContainer,
-		//speedContainer,
 		commandContainer,
 	)
 
@@ -186,9 +173,9 @@ func New(cpu *cpu.CPU, host *host.Host, submit func(), reset func(), load func()
 	)
 
 	w.SetContent(mainContainer)
-	consoleBuffer.Write([]byte("******************************************************************************************\n" +
-		"************************************* 6502 Simulator *************************************\n" +
-		"******************************************************************************************\n"))
+	consoleBuffer.Write([]byte("********************************************************************************\n" +
+		"******************************** 6502 Simulator ********************************\n" +
+		"********************************************************************************\n"))
 	consoleContainer.Refresh()
 	UpdateAll()
 
@@ -198,7 +185,6 @@ func New(cpu *cpu.CPU, host *host.Host, submit func(), reset func(), load func()
 func UpdateAll() {
 
 	// Reload
-	//inputCPUClock.SetText(fmt.Sprintf("%3f", c.Clock))
 	stackDisplay = c.GetStack()
 	stackLabelWidget.Text = stackDisplay
 	consoleDispString = consoleBuffer.String() // Get whatever is in the memory buffer from the host
@@ -208,7 +194,6 @@ func UpdateAll() {
 
 	// Refresh
 	buttonsContainer.Refresh()
-	//speedContainer.Refresh()
 	settingsContainer.Refresh()
 	stackLabelWidget.Refresh()
 	stackContainer.Refresh()
@@ -230,6 +215,7 @@ func Command() string {
 // Return the command line entered by user when Submit is pressed
 func ClearCmdLine() {
 	commandLine.SetText("")
+
 }
 
 func UpdateTime() {
